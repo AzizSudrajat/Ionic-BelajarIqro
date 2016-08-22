b8a910b80dd5ed3c46c5a1b87a8acd1817217bde
Aziz Sudrajat <azizsudrajat95@gmail.com>
2016-08-23 02:45:02 +0700

    tambah routing halaman5 - halaman31

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -78,7 +78,298 @@ app.config(function ($stateProvider, $urlRouterProvider) {
                 controller: 'HalamanCtrl'
             }
         }
-    });
+    })
+
+    .state('app.halaman3', {
+        url: '/halaman3',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman3.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman4', {
+        url: '/halaman4',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman4.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman5', {
+        url: '/halaman5',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman5.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman6', {
+        url: '/halaman6',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman6.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman7', {
+        url: '/halaman7',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman7.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman8', {
+        url: '/halaman8',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman8.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman9', {
+        url: '/halaman9',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman9.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman10', {
+        url: '/halaman10',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman10.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman11', {
+        url: '/halaman11',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman11.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman12', {
+        url: '/halaman12',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman12.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman13', {
+        url: '/halaman13',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman13.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman14', {
+        url: '/halaman14',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman14.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman15', {
+        url: '/halaman15',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman15.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman16', {
+        url: '/halaman16',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman16.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman17', {
+        url: '/halaman17',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman17.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman18', {
+        url: '/halaman18',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman18.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman19', {
+        url: '/halaman19',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman19.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman20', {
+        url: '/halaman20',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman20.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman21', {
+        url: '/halaman21',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman21.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman22', {
+        url: '/halaman22',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman22.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman23', {
+        url: '/halaman23',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman23.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman24', {
+        url: '/halaman24',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman24.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman25', {
+        url: '/halaman25',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman25.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman26', {
+        url: '/halaman26',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman26.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman27', {
+        url: '/halaman27',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman27.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman28', {
+        url: '/halaman28',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman28.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman29', {
+        url: '/halaman29',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman29.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman30', {
+        url: '/halaman30',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman30.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+
+    .state('app.halaman31', {
+        url: '/halaman31',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Halaman31.html',
+                controller: 'HalamanCtrl'
+            }
+        }
+    })
+    ;
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/belajariqro');
