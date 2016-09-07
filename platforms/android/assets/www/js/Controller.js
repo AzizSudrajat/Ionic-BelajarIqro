775da7ad043e9403c8d8eec8b837155f3cb82690
Aziz Sudrajat <azizsudrajat95@gmail.com>
2016-09-08 03:51:39 +0700

    push into origin master

--- platforms/android/assets/www/js/Controller.js
+++ platforms/android/assets/www/js/Controller.js
@@ -46,42 +46,129 @@ app.controller('TentangCtrl', function ($scope, $ionicPopup, $ionicPlatform) {
 // }, 100)
 })
 
-app.controller('HalamanCtrl', function ($scope, $ionicPlatform, $cordovaNativeAudio, $timeout) {
+app.controller('HalamanCtrl', function ($scope, $ionicPlatform, $cordovaNativeAudio) {
   $ionicPlatform.ready(function() {
-   $cordovaNativeAudio.preloadSimple('1', 'audio/Suara1.mp3')
-   $cordovaNativeAudio.preloadSimple('2', 'audio/Suara2.mp3')
-   $cordovaNativeAudio.preloadSimple('3', 'audio/Suara3.mp3')
-   $cordovaNativeAudio.preloadSimple('4', 'audio/Suara4.mp3')
-   $cordovaNativeAudio.preloadSimple('5', 'audio/Suara5.mp3')
-   $cordovaNativeAudio.preloadSimple('6', 'audio/Suara6.mp3')
-   $cordovaNativeAudio.preloadSimple('7', 'audio/Suara7.mp3')
-   $cordovaNativeAudio.preloadSimple('8', 'audio/Suara8.mp3')
-   $cordovaNativeAudio.preloadSimple('9', 'audio/Suara9.mp3')
-   $cordovaNativeAudio.preloadSimple('10', 'audio/Suara10.mp3')
-   $cordovaNativeAudio.preloadSimple('11', 'audio/Suara11.mp3')
-   $cordovaNativeAudio.preloadSimple('12', 'audio/Suara12.mp3')
-   $cordovaNativeAudio.preloadSimple('13', 'audio/Suara13.mp3')
-   $cordovaNativeAudio.preloadSimple('14', 'audio/Suara14.mp3')
-   $cordovaNativeAudio.preloadSimple('15', 'audio/Suara15.mp3')
-   $cordovaNativeAudio.preloadSimple('16', 'audio/Suara16.mp3')
-   $cordovaNativeAudio.preloadSimple('17', 'audio/Suara17.mp3')
-   $cordovaNativeAudio.preloadSimple('18', 'audio/Suara18.mp3')
-   $cordovaNativeAudio.preloadSimple('19', 'audio/Suara19.mp3')
-   $cordovaNativeAudio.preloadSimple('20', 'audio/Suara20.mp3')
-   $cordovaNativeAudio.preloadSimple('21', 'audio/Suara21.mp3')
-   $cordovaNativeAudio.preloadSimple('22', 'audio/Suara22.mp3')
-   $cordovaNativeAudio.preloadSimple('23', 'audio/Suara23.mp3')
-   $cordovaNativeAudio.preloadSimple('24', 'audio/Suara24.mp3')
-   $cordovaNativeAudio.preloadSimple('25', 'audio/Suara25.mp3')
-   $cordovaNativeAudio.preloadSimple('26', 'audio/Suara26.mp3')
-   $cordovaNativeAudio.preloadSimple('27', 'audio/Suara27.mp3')
-   $cordovaNativeAudio.preloadSimple('28', 'audio/Suara28.mp3')
-   $cordovaNativeAudio.preloadSimple('29', 'audio/Suara29.mp3')
-   .then(function (msg) {
-      console.log(msg);
-   }, function (error) {
-      console.log(error);
-   });
+   $cordovaNativeAudio.preloadSimple('1', 'audio/Suara1.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('2', 'audio/Suara2.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('3', 'audio/Suara3.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('4', 'audio/Suara4.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('5', 'audio/Suara5.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('6', 'audio/Suara6.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('7', 'audio/Suara7.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('8', 'audio/Suara8.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('9', 'audio/Suara9.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('10', 'audio/Suara10.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('11', 'audio/Suara11.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('12', 'audio/Suara12.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('13', 'audio/Suara13.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('14', 'audio/Suara14.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('15', 'audio/Suara15.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('16', 'audio/Suara16.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('17', 'audio/Suara17.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('18', 'audio/Suara18.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('19', 'audio/Suara19.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('20', 'audio/Suara20.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('21', 'audio/Suara21.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('22', 'audio/Suara22.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('23', 'audio/Suara23.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('24', 'audio/Suara24.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('25', 'audio/Suara25.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('26', 'audio/Suara26.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('27', 'audio/Suara27.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('28', 'audio/Suara28.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+   $cordovaNativeAudio.preloadSimple('29', 'audio/Suara29.mp3', function(msg){
+    }, function(msg){
+        console.log( 'error: ' + msg );
+    });
+  //  .then(function (msg) {
+  //     console.log(msg);
+  //  }, function (error) {
+  //     console.log(error);
+  //  });
 })
 
 $scope.play1 = function () {
