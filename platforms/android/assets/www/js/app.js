ee817855a20c7446ca2c2a2144d6c97088438b8f
AzizSudrajat <azizsudrajat95@gmail.com>
2016-09-18 18:38:12 +0700

    ubah tentang

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('app', ['ionic', 'ionic-material', 'ngCordova']);
+var app = angular.module('app', ['ionic', 'ionic-material', 'ngCordova','ui.router']);
 app.run(function ($ionicPlatform,$ionicPopup) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
